c2f11c538e438c262d7483f4ea95386fd53abfa1
Dick James <rnulanz@gmail.com>
2019-11-01 15:12:14 -0400

    added start script to package.json

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 require('dotenv').config()
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: process.env.mysql_User,
     password: process.env.mysql_Password,
     database: "burgers_db"
 });
-
+};
 connection.connect(function(err){
     if(err){
         console.log("Error connecting" + err.stack);
