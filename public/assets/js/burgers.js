a2bbb232f909061d716ba2064334e915232d3775
Dick James <rnulanz@gmail.com>
2019-10-31 21:31:05 -0400

    Added some style

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,52 +1,39 @@
 $(function() {
     // Add a new burger.
-    $(".create-form").on("submit", function(event) {
-        event.preventDefault();
+	$(".addBurgers").on("click", function(event){
+		event.preventDefault();
+		var id = $(this).data("id")
 
-        var newBurger = {
-            burger_name: $("#newburger").val().trim(),
-            devoured: 0
-        };
+		var newBurger = {
+			burger_name: $("#newBurgers").val().trim(),
+			devoured: 0
+		};
+		console.log(`${id} ${newBurger.burger_name} has been added `)
 
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(function() {
-            console.log("Added new burger");
-            // Reload the page to get the updated burger list.
-            location.reload();
-        });
-    });
+		$.ajax("/api/burgers", {
+			type: "POST",
+			data: newBurger
+		}).then(function(){
+			console.log(`Added new burger`)
+			//Reload the page to get updated burger list
+			location.reload();
+		})
+	})
 
-    $(".eatburger").on("click", function(event) {
-        event.preventDefault();
+	$(".notDevoured").on("click", function(){
+	
 
-        var id = $(this).data("id");
-        var devouredState = {
-            devoured: 1
-        };
+		var id = $(this).data("id");
+		var devouredState = {
+			devoured: 1
+		}
 
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: devouredState
-        }).then(function() {
-            console.log("Burger devoured");
-            location.reload();
-        });
-    });
-
-    $(".trashburger").on("click", function(event) {
-        event.preventDefault();
-
-        var id = $(this).data("id");
-
-        // Send the DELETE request.
-        $.ajax({
-            type: "DELETE",
-            url: "/api/burgers/" + id
-        }).then(location.reload());
-    });
-
+		$.ajax("/api/burgers/" + id, {
+			type: "PUT",
+			data: devouredState
+		}).then(function(){
+			console.log(`Burger has been devoured`);
+			location.reload();
+		})
+	});
 })
